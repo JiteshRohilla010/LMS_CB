e925c4cde39113bfcaafea1adb79f7be2444274d
Jitesh <jiteshrohilla2594@gmail.com>
2025-12-24 20:27:20 +0530

    first commit

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -15,14 +15,6 @@ const firebaseConfig = {
 };
 
 
-
-  // apiKey: "AIzaSyAHnHCjDMA8wsSsjpdlfzfaSYvPXOPuJXk",
-  // authDomain: "vrtualcourses.firebaseapp.com",
-  // projectId: "vrtualcourses",
-  // storageBucket: "vrtualcourses.firebasestorage.app",
-  // messagingSenderId: "288097336370",
-  // appId: "1:288097336370:web:bc417831deb9e56b7d102b"
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
