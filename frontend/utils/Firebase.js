d4340279a91ee17324c6bce179452e97ef402cb3
Jitesh <jiteshrohilla2594@gmail.com>
2025-12-24 16:08:06 +0530

    first commit

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,11 +2,16 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "vrtualcourses.firebaseapp.com",
-  projectId: "vrtualcourses",
-  storageBucket: "vrtualcourses.firebasestorage.app",
-  messagingSenderId: "288097336370",
-  appId: "1:288097336370:web:bc417831deb9e56b7d102b"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  // authDomain: "vrtualcourses.firebaseapp.com",
+  // projectId: "vrtualcourses",
+  // storageBucket: "vrtualcourses.firebasestorage.app",
+  // messagingSenderId: "288097336370",
+  // appId: "1:288097336370:web:bc417831deb9e56b7d102b"
 };
 
 
